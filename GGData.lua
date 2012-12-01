03c39ba3846031a72e36603d8679c7afe6500a51
Graham Ranson <graham@grahamranson.co.uk>
2012-12-01 21:04:55 +0000

    Added data integrity checks

--- GGData.lua
+++ GGData.lua
@@ -49,7 +49,54 @@ local GGData_mt = { __index = GGData }
 
 local json = require( "json" )
 local lfs = require( "lfs" )
+local crypto = require( "crypto" )
 --local sqlite3 = require( "sqlite3" )
+
+
+-------- Functions used for converting tables to strings which is used for data integrity. Functions sourced from here - http://lua-users.org/wiki/TableUtils
+function table.val_to_str ( v )
+	if "string" == type( v ) then
+		v = string.gsub( v, "\n", "\\n" )
+		if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+			return "'" .. v .. "'"
+		end
+		return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+	else
+		return "table" == type( v ) and table.tostring( v ) or tostring( v )
+	end
+end
+
+function table.key_to_str ( k )
+	if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+		return k
+	else
+		return "[" .. table.val_to_str( k ) .. "]"
+	end
+end
+
+function table.tostring( tbl )
+	local result, done = {}, {}
+	for k, v in ipairs( tbl ) do
+		table.insert( result, table.val_to_str( v ) )
+		done[ k ] = true
+	end
+	for k, v in pairs( tbl ) do
+		if not done[ k ] then
+	  		table.insert( result,
+			table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+		end
+	end
+	return "{" .. table.concat( result, "," ) .. "}"
+end
+
+local toString = function( value )
+	if type( value ) == "table" then
+		return table.tostring( value )
+	else
+		return tostring( value )
+	end
+end
+-----------------------------------------------
 
 --- Initiates a new GGData object.
 -- @param id The name of the GGData to create or load ( if it already exists ).
@@ -186,11 +233,15 @@ end
 --- Saves this GGData object to disk.
 function GGData:save()
 
+	-- Don't want this key getting saved out
+	local integrityKey = self.integrityKey
+	self.integrityKey = nil
+	
 	local data = {}
 	
 	-- Copy across all the properties that can be saved.
 	for k, v in pairs( self ) do
-		if type( v ) ~= "function" then
+		if type( v ) ~= "function" and type( v ) ~= "userdata" then
 			data[ k ] = v
 		end
 	end
@@ -231,6 +282,9 @@ function GGData:save()
 		
 	end
 	
+	-- Set the key back again
+	self.integrityKey = integrityKey
+	
 end
 
 --- Sets a value in this GGData object.
@@ -238,6 +292,7 @@ end
 -- @param value The value to set.
 function GGData:set( name, value )
 	self[ name ] = value
+	self:storeIntegrityHash( name, value )
 end
 
 --- Gets a value from this GGData object.
@@ -293,6 +348,7 @@ end
 function GGData:setIfNew( name, value )
 	if self[ name ] == nil then
 		self[ name ] = value
+		self:storeIntegrityHash( name, value )
 	end
 end
 
@@ -302,6 +358,7 @@ end
 function GGData:setIfHigher( name, value )
 	if self[ name ] and value > self[ name ] or not self[ name ] then
 		self[ name ] = value
+		self:storeIntegrityHash( name, value )
 	end
 end
 
@@ -311,6 +368,7 @@ end
 function GGData:setIfLower( name, value )
 	if self[ name ] and value < self[ name ] or not self[ name ] then
 		self[ name ] = value
+		self:storeIntegrityHash( name, value )
 	end
 end
 
@@ -320,6 +378,7 @@ end
 function GGData:increment( name, amount )
 	if self[ name ] and type( self[ name ] ) == "number" then
 		self[ name ] = self[ name ] + ( amount or 1 )
+		self:storeIntegrityHash( name, value )
 	end
 end
 
@@ -329,6 +388,7 @@ end
 function GGData:decrement( name, amount )
 	if self[ name ] and type( self[ name ] ) == "number" then
 		self[ name ] = self[ name ] - ( amount or 1 )
+		self:storeIntegrityHash( name, value )
 	end
 end
 
@@ -385,6 +445,109 @@ function GGData:getSync( id )
 	end
 	
 	return native.getSync( "boxes/" .. id .. ".box", { key = "iCloudBackup" } )
+	
+end
+
+--- Enables integrity checking.
+-- @param algorithm The hashing algorithm to use, see this page for possibles - http://docs.coronalabs.com/api/library/crypto/index.html
+-- @param key The seed to use for the hashing algorithm.
+function GGData:enableIntegrityControl( algorithm, key )
+	self.integrityAlgorithm = algorithm
+	self.integrityKey = key
+	self.hash = self.hash or {}
+	self.integrityControlEnabled = true
+end
+
+--- Disables integrity checking.
+function GGData:disableIntegrityControl()
+	self.integrityAlgorithm = nil
+	self.integrityKey = nil
+	self.hash = nil
+	self.integrityControlEnabled = false
+end
+
+--- Verifies that the passed in value matches the stored hash.
+-- @param name The name of the value to check.
+-- @param value The value to check.
+-- @return True if the value matches the hash false otherwise.
+function GGData:verifyItemIntegrity( name, value )
+	-- just hash the tostring() version and compare against that!
+	if toString( value ) then
+		local generatedHash = crypto.hmac( self.integrityAlgorithm, toString( value ), self.integrityKey, false )
+		local storedHash = self.hash[ name ]
+		return generatedHash == storedHash
+	end
+end
+
+--- Stores the hash value of the given value to be used for integrity checks.
+-- @param name The name of the value to set.
+-- @param value The value to set. Optional, if not included will just pull the value from the name supplied.
+function GGData:storeIntegrityHash( name, value )
+
+	if not self.integrityControlEnabled then
+		return
+	end
+	
+	value = value or self[ name ]
+	
+	self.hash = self.hash or {}
+	
+	if value then
+		self.hash[ name ] = crypto.hmac( self.integrityAlgorithm, toString( value ), self.integrityKey, false )
+	end
+	
+end
+
+--- Updates/sets the hash value of the all stored values.
+function GGData:updateAllIntegrityHashes()
+	
+	for k, v in pairs( self ) do
+		if k ~= "integrityControlEnabled" 
+			and k ~= "integrityAlgorithm"
+			and k ~= "integrityKey"
+			and k ~= "hash" 
+			and k ~= "id" 
+			and  toString( v ) then
+				self:storeIntegrityHash( k, v )
+		end
+	end
+	
+end
+
+--- Checks the hashed versions of all stored data. Will remove any values that don't match their hashes, i.e. they've been tampered with.
+function GGData:verifyIntegrity()
+	
+	if not self.integrityControlEnabled then
+		return
+	end
+	
+	local corruptEntries = {}
+	
+	for k, v in pairs( self ) do
+		if k ~= "integrityControlEnabled" 
+			and k ~= "integrityAlgorithm"
+			and k ~= "integrityKey"
+			and k ~= "hash" 
+			and k ~= "id" 
+			and toString( v ) then
+				
+				if not self:verifyItemIntegrity( k, v ) then
+					corruptEntries[ #corruptEntries + 1 ] = { name = k, value = v }
+					self[ k ] = nil
+					self.hash[ k ] = nil
+				end
+		end
+	end
+	
+	for k, v in pairs( self.hash ) do
+		if not self[ k ] then
+			corruptEntries[ #corruptEntries + 1 ] = { name = k, value = v }
+			self[ k ] = nil
+			self.hash[ k ] = nil
+		end
+	end
+	
+	return corruptEntries
 	
 end
 
--- README.md
+++ README.md
@@ -43,6 +43,30 @@ box:setSync( false )
 print( box:getSync() )
 ```
 
+##### Enable integrity checking. This should be set BEFORE adding values and needs to be set on each run as the key is not stored ( as it would defeat the point ). 
+```lua
+local crypto = require( "crypto" )
+box:enableIntegrityControl( crypto.sha512, "SECRET_KEY" )
+```
+
+##### If you decide to enable integrity checking after you have already set a bunch of values you will want to update all the hashes.
+```lua
+box1:enableIntegrityControl( crypto.sha512, "MONKEY" )
+box1:updateAllIntegrityHashes()
+```
+
+##### When adding or editing values via any of the helper methods integrity data will be stored automatically but if you store the data manually you will also need to add this extra data.
+```lua
+box.newValue = "Hello, World!"
+box:storeIntegrityHash( "newValue" )
+```
+
+##### Verify the integrity of all items. Any values that are detected as wrong will be nilled out. Remember to save after doing this :-)
+```lua
+local corruptEntries = box1:verifyIntegrity()
+print( corruptEntries ) -- How many, if any, values were removed due to being different than expected.
+```
+
 Update History
 -------------------------
 
